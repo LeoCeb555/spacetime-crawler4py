89120ba11aa23a91601e20e01b9f80b11fef90c5
Leonardo Cebrero <cebrerol@uci.edu>
2026-02-08 09:38:11 -0800

    Integrated final report variables. Cleaned up formatting for prints. Function done.

--- report.py
+++ report.py
@@ -1,28 +1,16 @@
-from scraper import longestLength, longestURL, word_frequencies, numOfUniquePages
-# I don't think we need to have this done rn, as long as our
-# crawler is actually running by deployment date
-
-# as far as this goes I thought It'd be easier to just createa a function
-# that prints out everything we need
-
-#1. Unique pages - we just have to create a list of unique urls
-# and loop through the lines in analytics_data and skip empty/duplicate
-# urls - DONE
-
-#2. Loop and Compare word count of current URL to a 'longest' variable
-# and if its > then just update it to the new longest one - DONE
-
-#3. I think common words is pretty easy, we already did something similar
-# in assignment 1.  - DONE
-
-#4. Gotta do some parsing here I assume, lowkey I gotta go to work tonight
-# so I'm going to leave this for later unless you want to try it rn
+from scraper import longestLength, longestURL, word_frequencies, unique_urls, numOfUniquePagesPerSubDomain
 
 def makeReport():
     #Num of unique pages found
-    print("Number of unique pages found: " + str(numOfUniquePages) + "\n")
+    print("Number of unique pages found: " + str(len(unique_urls)) + "\n")
     #Longest page
     print("Longest page: " + longestURL + " - " + str(longestLength) + " words\n")
     #Top 50 most common words
     print("50 most common words: ")
-    print(word_frequencies.most_common(50))
+    for word, freq in word_frequencies.most_common(50):
+        print(f"{word}: {freq}")
+    #Number of subdomains and unique pages per subdomain
+    print("Number of subdomains:", len(numOfUniquePagesPerSubDomain))
+    print("Number of unique pages per subdomain:\n")
+    for subdomain in sorted(numOfUniquePagesPerSubDomain):
+        print(f"{subdomain}, {numOfUniquePagesPerSubDomain[subdomain]}")
